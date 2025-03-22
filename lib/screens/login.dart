6aaae50eff15723a1f9cf26ceff7994e6d376dfa
Jereme <jeremejazz15@gmail.com>
2025-03-22 20:25:42 +0800

    chore: implement basic sign up feature; allow internet for macos desktop

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,6 +11,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,15 +1,91 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_example/components/auth_button.dart';
 import 'package:flutter_auth_example/components/auth_textfield.dart';
+import 'package:flutter_auth_example/helpers/showsnackbar.dart';
+import 'package:http/http.dart' as http;
 
 class SignUp extends StatelessWidget {
   SignUp({super.key});
   final fullnameController = TextEditingController();
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  Future<void> _signUp(BuildContext context) async {
+    FocusScope.of(context).unfocus();
+
+    final String fullName = fullnameController.text.trim();
+    final String username = usernameController.text.trim();
+    final String password = passwordController.text.trim();
+
+    if (fullName.isEmpty || username.isEmpty || password.isEmpty) {
+      showSnackBar(context, 'Please fill in all fields.', Colors.red);
+      return;
+    }
+
+    if (password.length < 6) {
+      showSnackBar(
+        context,
+        'Password must be at least 6 characters',
+        Colors.red,
+      );
+      return;
+    }
+
+    final Map<String, String> signUpData = {
+      'fullName': fullName,
+      'username': username,
+      'password': password,
+    };
+
+    try {
+      var urlString = 'http://localhost:3000/auth/signup';
+
+      if (Platform.isAndroid) {
+        urlString = 'http://10.0.2.2:3000/auth/signup';
+      }
+
+      final url = Uri.parse(urlString);
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: jsonEncode(signUpData),
+          )
+          .timeout(const Duration(seconds: 10));
+
+      if (response.statusCode == 201) {
+        if (context.mounted) {
+          showSnackBar(context, 'Sign-up successful!', Colors.black);
+          Navigator.pop(context);
+        }
+      } else {
+        final responseData = json.decode(response.body);
+        final errorMessage = responseData['message'] ?? 'Sign-up failed.';
+
+        if (context.mounted) showSnackBar(context, errorMessage, Colors.red);
+      }
+    } catch (error) {
+      String errorMessage = 'An error occured during sign-up.';
+      if (error is http.ClientException || error is SocketException) {
+        errorMessage =
+            'Failed to connect to the Server. Check your internet connection.';
+      } else if (error is TimeoutException) {
+        errorMessage = 'Connection timed out. Please try again later.';
+      }
+
+      debugPrint('Error ${error.toString()}');
+      if (context.mounted) showSnackBar(context, errorMessage, Colors.red);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[300],
       appBar: AppBar(backgroundColor: Colors.grey[300]),
       body: SafeArea(
@@ -40,26 +116,7 @@ class SignUp extends StatelessWidget {
               ),
               const SizedBox(height: 25.0),
               AuthButton(
-                onTap: () async {
-                  try {
-                    if (fullnameController.text.isEmpty ||
-                        usernameController.text.isEmpty ||
-                        passwordController.text.isEmpty) {
-                      throw Exception('All fields are required.');
-                    }
-                    await Future.delayed(const Duration(seconds: 2));
-                    debugPrint('Sign Up Successful');
-                  } catch (e) {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        (SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text('Sign Up Failed: ${e.toString()}'),
-                        )),
-                      );
-                    }
-                  }
-                },
+                onTap: () => _signUp(context),
                 label: 'Create Account',
               ),
               const SizedBox(height: 50.0),
